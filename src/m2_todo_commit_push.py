4781fb40ccaf6446dbb0f730353ba2a74578e003
lpacas001 <lpacas001@student.franciscan.edu>
2024-01-23 12:06:25 -0500

    FInished todo

--- src/m2_todo_commit_push.py
+++ src/m2_todo_commit_push.py
@@ -92,7 +92,7 @@ print(product)
 
 ###############################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Look at the list of files in the Project window (to the left).  Note that
 #   this file (m2_todo_and_commit_push.py) now has a 'U' next to it.
 #
